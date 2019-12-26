da47884be6933440c976c15ef8e9cec386634af6
faheemsaleem <fah03134508455@gmail.com>
2019-12-26 11:33:26 +0500

    Updates

---
+++ precache-manifest.4bcaa00f2dba0fe7b2f6dcd5c295ea24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8192068324fa565b8b87f82adb239aa5",
+    "url": "/rebot-app/index.html"
+  },
+  {
+    "revision": "25e239f1988ec387df53",
+    "url": "/rebot-app/static/css/main.654a9aec.chunk.css"
+  },
+  {
+    "revision": "75d3d075bc2b8257e158",
+    "url": "/rebot-app/static/js/2.cb808454.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rebot-app/static/js/2.cb808454.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25e239f1988ec387df53",
+    "url": "/rebot-app/static/js/main.cde1d63e.chunk.js"
+  },
+  {
+    "revision": "3cea1682a91ce23c99da",
+    "url": "/rebot-app/static/js/runtime-main.48ab49ab.js"
+  }
+]);
